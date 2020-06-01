ab096a27deda3044b2d0104c47cea91f5d78b62a
Rejon <rejon@maximumcrash.com>
2020-06-01 13:50:50 -0400

    Got a rudimentary approach to dynamically generated header links based on top level directories of the content folder.

--- src/modules/layouts/site_layout.js
+++ src/modules/layouts/site_layout.js
@@ -7,7 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+
 import { MDXProvider } from "@mdx-js/react"
 import Header from "../ui/header"
 import Breadcrumbs from '@modules/ui/Breadcrumbs'
@@ -15,18 +15,10 @@ import Sidenav from '@modules/ui/Sidenav'
 const shortcodes = {Breadcrumbs};
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header />
       <div
         style={{
           margin: `0 auto`,
--- src/modules/ui/header.js
+++ src/modules/ui/header.js
@@ -4,32 +4,66 @@ import PropTypes from "prop-types"
 import React from "react"
 import {jsx} from 'theme-ui'
 import { Icon } from "@makerdao/dai-ui-icons";
+import { useStaticQuery, graphql } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <header sx={{bg: 'primary', marginBottom: '1.45rem'}}>
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-        display: 'flex',
-        'alignItems': 'center'
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Header = () => {
+  const {site, allDirectory} = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+
+      allDirectory(filter: {absolutePath: {regex: "/\/content\/([^\/]+)[^\/]$/"}}) {
+        nodes {
+          absolutePath
+        }
+      }
+    }
+  `)
+
+
+  const headerLinks = allDirectory.nodes.map(n => {
+      const directoryPath = n.absolutePath.split("/");
+      let urlPath = directoryPath[directoryPath.length-1];
+      let linkLabel = urlPath.replace(/_|-|\./g, ' ');
+
+      return (<Link to={urlPath} sx={{textTransform: 'capitalize', mr: 2}}><p>{linkLabel}</p></Link>)
+  });
+
+  const siteTitle =site.siteMetadata.title;
+
+  return (
+    <header sx={{bg: 'primary', marginBottom: '1.45rem'}}>
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+          display: 'flex',
+          'alignItems': 'center'
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+        <div>
+          {headerLinks}
+        </div>
+      </div>
+    </header>
+  )
+  
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
