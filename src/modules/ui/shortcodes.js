a247e18083be69ef0e7cf04b76b5f32501ed0c04
Rejon <rejon@maximumcrash.com>
2020-06-03 12:32:59 -0400

    Added MDX image support. Updated shortcodes with theme-ui text components. Added starfox for example.

--- src/modules/ui/shortcodes.js
+++ src/modules/ui/shortcodes.js
@@ -4,5 +4,19 @@ import Breadcrumbs from "@modules/ui/Breadcrumbs";
 import Sidenav from "@modules/ui/Sidenav";
 import StatusBanner from '@modules/ui/StatusBanner';
 import Link from "@modules/utility/Link"
+import {Image, Text, Divider} from 'theme-ui'
 
-export default { Breadcrumbs, StatusBanner, Sidenav, a: props => <Link to={props.href} {...props}/> }; 
+export default {
+	a: props => <Link to={props.href} {...props}/>,
+	img: props => <Image {...props}/>,
+	h1: props => <Text variant="h1" {...props}/>,
+	h2: props => <Text variant="h2" {...props}/>,
+	h3: props => <Text variant="h3" {...props}/>,
+	h4: props => <Text variant="h4" {...props}/>,
+	h5: props => <Text variant="h5" {...props}/>,
+	thematicBreak: props => <Divider/>,
+	p: props => <Text {...props}/>,
+	Breadcrumbs,
+	StatusBanner,
+	Sidenav 
+}; 
