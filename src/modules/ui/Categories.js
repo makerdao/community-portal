a0a972ed6fea9386afd1edea67e8388453eee44c
Elijah Baldwin <befitsandpiper@gmail.com>
2020-06-28 17:29:01 -0400

    fix grid spacing, just use plain css grids

--- src/modules/ui/Categories.js
+++ src/modules/ui/Categories.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import React, { Fragment } from 'react';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
-import { jsx, Grid } from 'theme-ui';
+import { jsx } from 'theme-ui';
 import { useLocation } from '@reach/router';
 import { useStaticQuery, graphql } from 'gatsby';
 
@@ -9,35 +9,69 @@ import useTranslation from '@modules/utility/useTranslation';
 import Link from '@modules/utility/Link';
 
 const Categories = ({ children }) => {
-    const lengthChildren = children.length;
-    let columnString = '1fr 1fr';
-    let columns = 2;
-    let width = '480px';
-    if (lengthChildren === 3 || lengthChildren > 4) {
-        width = '361px';
-        columnString = '1fr 1fr 1fr';
-        columns = 3;
-    } else if (lengthChildren === 1) {
-        columnString = '1fr';
-        columns = 1;
-    }
+  const lengthChildren = children.length;
+  let columns = 1;
+  let columns2 = 0;
 
-    const boxStyle = {
-        // width,
+  let width = '480px';
+  if (lengthChildren === 3 || lengthChildren > 4) {
+    width = '361px';
+    columns = 3;
+    columns2 = lengthChildren % 3;
+  } else if (lengthChildren === 4) {
+    columns = 2;
+    columns2 = 2;
+  }
+
+  const columnString = 'auto '.repeat(columns - 1) + 'auto';
+  const column2String = columns2 ? 'auto '.repeat(columns2 - 1) + 'auto' : '';
+
+  const childrenJSX = children.map((child, ind) => (
+    <div
+      sx={{
+        width,
         height: '260px',
         background: '#FFFFFF',
         border: '1px solid rgba(41, 26, 66, 0.1)',
         boxSizing: 'border-box',
         borderRadius: '4px'
-    };
+      }}
+      key={ind}
+      bg="muted"
+    >
+      <div sx={{ padding: '37px' }}>{child}</div>
+    </div>
+  ));
 
-    const childrenJSX = children.map((child, ind) => (
-        <div style={boxStyle} key={ind} bg="muted">
-            <div style={{ padding: '37px' }}>{child}</div>
+  const containerStyles = {
+    display: 'grid',
+    gridGap: '10px',
+    marginTop: '10px',
+    justifyContent: 'center'
+  };
+
+  return (
+    <>
+      <div
+        sx={{
+          ...containerStyles,
+          gridTemplateColumns: columnString
+        }}
+      >
+        {childrenJSX.slice(0, lengthChildren - columns2)}
+      </div>
+      {columns2 && (
+        <div
+          sx={{
+            ...containerStyles,
+            gridTemplateColumns: column2String
+          }}
+        >
+          {childrenJSX.slice(lengthChildren - columns2)}
         </div>
-    ));
-
-    return <Grid columns={[columns, columnString]}>{childrenJSX}</Grid>;
+      )}
+    </>
+  );
 };
 
 export default Categories;
