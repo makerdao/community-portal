aa4855d30a31c5fc7d73bf23d57006c06cad584b
Isaac Pearl <pearl.isaac@gmail.com>
2020-06-28 20:15:42 -0400

    fixed spacing issues

--- src/modules/ui/Chocolate.js
+++ src/modules/ui/Chocolate.js
@@ -1,86 +1,65 @@
 /** @jsx jsx */
 import React from "react";
 import { jsx, Box, Grid } from "theme-ui";
-import { Icon } from "@makerdao/dai-ui-icons";
 
 const Chocolate = ({ children }) => {
-  const _Children = React.Children.toArray(children);
-  
-  let leftovers = _Children.slice(Math.max(_Children.length - (_Children.length % 3), 1));
+  const _Children = React.Children.toArray(children); // convert children to array
 
-  console.log(`children length: ${_Children.length}`);
+  // grid column values
+  let maxBoxSize = 245;
+  let minBoxSize = 100;
 
-  let containerStyle;
-  let leftoversStyle;
+  // default grid sx prop
+  let containerStyle = {
+      columnGap: "32px",
+      gridTemplateColumns: `minmax(${minBoxSize}px, ${maxBoxSize}px) minmax(${minBoxSize}px, ${maxBoxSize}px) minmax(${minBoxSize}px, ${maxBoxSize}px)`
+  };
+
+  // the number of children on the last row, if not full
+  let leftovers = _Children.length % 3;
 
   if (_Children.length === 1) {
-    leftovers = [];
-    containerStyle = {
-      gridTemplateColumns: "1fr"
-    };
+    leftovers = 0; // to prevent empty box from being added to last row
   } else if (_Children.length === 4 || _Children.length === 2) {
-    leftovers = [];
+    leftovers = 0; // to prevent empty box from being added to last row
     containerStyle = {
-      gridTemplateColumns: "1fr 1fr",
-      justifyItems: "center"
-    };
-  } else {
-    containerStyle = {
-      justifyItems: "center",
-      gridTemplateColumns: "1fr 1fr 1fr",
+      columnGap: "32px",
+      gridTemplateColumns: `minmax(${minBoxSize}px, ${maxBoxSize}px) minmax(${minBoxSize}px, ${maxBoxSize}px)` // two columns special case
     };
   }
 
-  if (leftovers.length === 1) {
-    leftoversStyle = {
-      justifyItems: "center",
-      gridTemplateColumns: "1fr"
-    };
-  } else if (leftovers.length === 2)  {
-    leftoversStyle = {
-      justifyItems: "center",
-      gridTemplateColumns: "1fr 1fr"
-    };
-  }
+  // if the last row only has one item, create an empty box to its left so the leftover box can be centered
+  if (leftovers === 1) {
+    _Children.splice(-1,0,
+      <Box 
+        sx = {{
+          mb: "16px",
+          mr: "16px"
+        }} 
+      />
+    );
+  };
 
   return (
-    <>
     <Grid
       sx={containerStyle}
     >
-        {_Children.map((child, index) => {
-        if (leftovers.length === 0 || index <  _Children.length - (_Children.length % 3)) {
+      {_Children.map((child, index) => {
           return (
             <Box 
+              key = {index}
               sx = {{
                 mb: "16px",
-                maxWidth: "245px",
+                mr: "16px"
               }} 
             >
               {child}
             </Box>
           );
         }
-      })}
+      )}
     </Grid>
-      {leftovers.length > 0  &&
-    <Grid sx={leftoversStyle}>
-      {leftovers.map((child, index) => {
-        return (
-          <Box 
-            sx = {{
-              mb: "16px",
-              maxWidth: "245px"
-            }}
-          >
-            {child}
-          </Box>
-        );
-      })}
-    </Grid>
-        }
-    </>
-  );
+  )
 };
 
 export default Chocolate;
