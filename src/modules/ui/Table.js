40b9d82f96168bc6950336340cefb7e112cd4ce7
Elijah Baldwin <befitsandpiper@gmail.com>
2020-07-08 19:40:22 -0400

    remove return

--- src/modules/ui/Table.js
+++ src/modules/ui/Table.js
@@ -4,31 +4,29 @@ import { jsx } from "theme-ui";
 
 const Table = ({
   children,
-}) => {
-  return (
-    <table
-      sx={{
-        borderCollapse: "collapse",
-        borderSpacing: "5px",
-        "& td": {
-          border: "1px solid black",
-          padding: "5px",
-          textAlign: "left",
-        },
-        "tr:nth-child(even)": {
-          backgroundColor: "#f2f2f2",
-        },
-        "& th": {
-          border: "1px solid black",
-          padding: "20px",
-          fontSize: "18px",
-          textAlign: "center",
-        },
-      }}
-    >
-      {children}
-    </table>
-  );
-};
+}) => (
+  <table
+    sx={{
+      borderCollapse: "collapse",
+      borderSpacing: "5px",
+      "& td": {
+        border: "1px solid black",
+        padding: "5px",
+        textAlign: "left",
+      },
+      "tr:nth-child(even)": {
+        backgroundColor: "#f2f2f2",
+      },
+      "& th": {
+        border: "1px solid black",
+        padding: "20px",
+        fontSize: "18px",
+        textAlign: "center",
+      },
+    }}
+  >
+    {children}
+  </table>
+);
 
 export default Table;
