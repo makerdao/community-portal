f10f957dd990fbd803ae35fce5f1b2ae6fb32339
Rejon <rejon@maximumcrash.com>
2020-07-22 21:29:34 -0400

    Finished InfoBlock styles.

--- src/modules/ui/InfoBlock.js
+++ src/modules/ui/InfoBlock.js
@@ -3,125 +3,70 @@ import React from "react";
 import { Box, Flex, jsx } from "theme-ui";
 
 //TODO(Rejon): On mobile have image ALWAYS stack in order first. Do this when we have breakpoints figured out.
-const InfoBlock = ({ children, alt, backgroundImage, minImageWidth, sx }) => {
+const InfoBlock = ({ children, sx }) => {
   let _Children = React.Children.toArray(children);
+  let imageIndex = 0;
+  let hasImageMobileOrdering = {};
 
-  const getImageData = () => {
-    const checkIfElementIsImage = (el) => {
-      //NOTE(Rejon): This looks like a hack, but because gatsby-remark-images blackboxes us from editing classnames
-      //			   AND they change the element to be spans + a, there's no clean way to find an image.
-      //			   Thus, we're dependent on the consistency of the classname gatsby-resp-image-wrapper.
+  //NOTE(Rejon): In the future when css4 comes out this will be replaced by the :has() selector!
+  const hasImage = _Children.find((n, index) => {
+    const _isTrue = n.props.mdxType === 'Image' || (n.props.children && !Array.isArray(n.props.children) && n.props.children.props.mdxType === 'img');
 
-      //If we're using an Image component directly in code, className will be top level.
-      if (el.props.className === "gatsby-resp-image-wrapper") {
-        return true;
-      }
-      //If we're using MD image scripting, it will be a little deeper.
-      else if (typeof el.props.children === "object") {
-        if (el.props.children.props.className === "gatsby-resp-image-wrapper") {
-          return true;
-        }
-      }
-
-      return false;
-    };
-
-    //This is to ensure that images don't shrink lower than expected.
-    //NOTE(Rejon): I do this because when we shrink the screen, if the image doesn't
-    // 			   have a min width it will shrink instead of the copy besides it.
-    const getElementMinSize = (img) => {
-      if (img.props.className === "gatsby-resp-image-wrapper") {
-        return img.props.style.maxWidth;
-      }
-      //If we're using MD image scripting, it will be a little deeper.
-      else if (typeof img.props.children === "object") {
-        if (
-          img.props.children.props.className === "gatsby-resp-image-wrapper"
-        ) {
-          return img.props.children.props.style.maxWidth;
-        }
-      }
-    };
-
-    if (checkIfElementIsImage(_Children[0])) {
-      //Image is FIRST
-      const img = _Children[0];
-      _Children.splice(0, 1);
-
-      return {
-        img,
-        first: true,
-        minWidth: minImageWidth || getElementMinSize(img),
-      };
-    } else if (checkIfElementIsImage(_Children[_Children.length - 1])) {
-      //Image is LAST
-      const img = _Children[_Children.length - 1];
-      _Children.splice(-1);
-
-      return {
-        img,
-        first: false,
-        minWidth: minImageWidth || getElementMinSize(img),
-      };
-    } else {
-      //NO Image is First OR Last
-      return null;
+    if (_isTrue) {
+      imageIndex = index; 
+      return _isTrue;
     }
-  };
+  });
 
-  const imageData = getImageData();
+  if (hasImage && imageIndex !== 0) {
+    hasImageMobileOrdering[`& > *:nth-child(${imageIndex + 1})`] = {
+        order: [0,0,1]
+    }
 
-  const NoImageStyles =
-    imageData === null
-      ? {
-          width: "100%",
-          textAlign: "center",
-          "& .gatsby-resp-image-wrapper": {
-            width: "100% !important",
-            margin: "auto !important",
-          },
-        }
-      : {};
+    hasImageMobileOrdering[`& > *:not(:nth-child(${imageIndex + 1}))`] = {
+      order: [1,1,0]
+    }
+  }
 
+  console.log(hasImage, hasImageMobileOrdering);
   return (
     <Flex
       sx={{
         justifyContent: "center",
         alignItems: "center",
-        background: `url(${backgroundImage})`,
-        ...sx,
+        flexDirection: ['column', 'column', 'row'],
+        width: "100%",
+        px: ['20px', '20px', '10.5%'],
+        py: ['60px', '60px', '72px'],
+        textAlign: hasImage ? '' : 'center',
+        '& > *': {
+          width: ['100%', '100%', '50%'],
+          minWidth: ['unset', 'unset', '300px'],
+          mb: 0,
+          margin: hasImage ? '' : 'auto',
+          '& > *' : {
+            mx: hasImage ? '' : 'auto'
+          }
+        },
+        '& > * img': {
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          objectPosition: 'center',
+          
+        },
+        '& > *:first-child': {
+          minWidth: ['unset', 'unset', '250px'],
+          mr: hasImage ? [0,0,'4%'] : 0
+        },
+        '& > *:last-child': {
+          minWidth: ['unset', 'unset', '250px'],
+          ml: hasImage ? [0,0,'4%'] : 0
+        },
+        ...hasImageMobileOrdering
       }}
     >
-      {imageData && (
-        <Box
-          sx={{
-            order: imageData.first ? 0 : 1,
-            mr: imageData.first && !alt ? 4 : 0,
-            flex: alt ? "1" : "1 0 0%",
-            flexGrow: "0",
-            minWidth: alt ? "50%" : imageData.minWidth,
-            maxWidth: alt ? "50%" : "40%",
-            "& > *": { m: 0 },
-            "& .gatsby-resp-image-wrapper": alt
-              ? { maxWidth: "unset !important", width: "100%" }
-              : {},
-          }}
-        >
-          {imageData.img}
-        </Box>
-      )}
-      <Box
-        sx={{
-          order: imageData && imageData.first ? 1 : 0,
-          width: "auto",
-          margin: "auto 0",
-          pr: 3,
-          pl: alt ? 3 : 0,
-          ...NoImageStyles,
-        }}
-      >
         {_Children.map((child) => child)}
-      </Box>
     </Flex>
   );
 };
