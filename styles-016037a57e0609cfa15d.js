8dfa1ac5c89341f3e773d0120b51aefa415460c1
twblack88 <timothy.william.black@gmail.com>
2020-08-28 04:33:43 +0000

    Deploying to gh-pages from  @ 54ca3e5f6f94e319e402de0358baf8f9904416c0 🚀

---
+++ styles-016037a57e0609cfa15d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[114],[]]);
+//# sourceMappingURL=styles-016037a57e0609cfa15d.js.map
--- sw.js
+++ sw.js
@@ -31,32 +31,32 @@ self.__precacheManifest = [
     "revision": "52174acfbd2177fa8002355d9b50f850"
   },
   {
-    "url": "framework-f05b33d929e4dca22752.js"
+    "url": "framework-b17c82fe3e340337cc56.js"
   },
   {
-    "url": "styles.a1d961b5dd920f0d1404.css"
+    "url": "styles.2e5c0bd762dde897d517.css"
   },
   {
-    "url": "styles-030c3638f3772f22a80b.js"
+    "url": "styles-016037a57e0609cfa15d.js"
   },
   {
     "url": "05d954cf-9a5ece01423d6dd1342c.js"
   },
   {
-    "url": "app-b2cf37db1843b548abc1.js"
+    "url": "app-d8bbd1ac3b285732177b.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "fe49e2b9ea89e2b8e66fdda36573afcf"
+    "revision": "5b5c001074d41cd0c9924a00057b000a"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
   },
   {
-    "url": "webpack-runtime-eb336ed62018b2617378.js"
+    "url": "webpack-runtime-33140e410051f58d513b.js"
   },
   {
-    "url": "polyfill-396b850cdd762ed99fa4.js"
+    "url": "polyfill-fbce1c89e648a4ea19cd.js"
   },
   {
     "url": "manifest.webmanifest",
@@ -152,7 +152,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-b2cf37db1843b548abc1.js`))) {
+  if (!resources || !(await caches.match(`/app-d8bbd1ac3b285732177b.js`))) {
     return await fetch(event.request)
   }
 
